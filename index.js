6db7b8f9d15a283b759b898691e56e06d4db952a
Anthony Carrillo <carrilloanthony2698@gmail.com>
2020-07-09 11:17:22 -0400

    Completed middleware, working on endpoints.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code away!
+const server = require("./server");
+
+const PORT = 4000;
+
+server.listen(PORT, () => {
+    console.log(`\n <= Server running on port ${PORT}! => \n`);
+});
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const userDB = require("./userDb");
+const postDB = require("../posts/postDb");
 
 const router = express.Router();
+
+router.use("/:id", validateUserId); 
 
 router.post('/', (req, res) => {
   // do your magic!
@@ -11,37 +15,90 @@ router.post('/:id/posts', (req, res) => {
 });
 
 router.get('/', (req, res) => {
-  // do your magic!
+    userDB.get()
+        .then(response => {
+            res.status(200).json({ response })
+        })
+        .catch(error => {
+            res.status(500).json({ message: "Unable to retrieve users." });
+        })
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+    userDB.getById(req.params.id)
+        .then(response => {
+            res.status(200).json({ response });
+        })
+        .catch(error => {
+            res.status(500).json({ message: "Unable to retrive user." })
+        })
 });
 
 router.get('/:id/posts', (req, res) => {
-  // do your magic!
+    userDB.getUserPosts(req.params.id)
+        .then(response => {
+            res.status(200).json({ response });
+        })
+        .catch(error => {
+            res.status(500).json({ errorMessage: "Unable to retrieve user posts." });
+        })
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+    userDB.remove(req.params.id)
+        .then(response => {
+            res.status(200).json({ message: `${response} user was deleted from database.` });
+        })
+        .catch(error => {
+            res.status(500).json({ errorMessage: "Can't remove user." });
+        });
 });
 
 router.put('/:id', (req, res) => {
-  // do your magic!
+    userDB.update(req.params.id, req.body)
+        .then(response => {
+            res.status(200).json({ message: `${response} user was updated.` });
+        })
+        .catch(error => {
+            res.status(500).json({ errorMessage: "Can't update user." });
+        })
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+    userDB.getById(req.params.id)
+        .then(response => {
+            if ( response ) {
+                req.user = response;
+                next();
+            } else {
+                res.status(404).json({ message: "Invalid user id." })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ message: "Unable to connect to database." });
+        });
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+    if ( !req.body ) {
+        res.status(400).json({ message: "Missing user data." });
+    } else if ( !req.body.name ) {
+        res.status(400).json({ message: "Missing required name field." });
+    } else {
+        next();
+    }
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  if ( !req.body ) {
+      res.status(400).json({ message: "Missing user data." });
+  } else if ( !req.body.text ) {
+      res.status(400).json({ message: "Missing required name text." });
+  } else {
+    next();
+  } 
 }
 
 module.exports = router;
